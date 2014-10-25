14ed625e12062ee8dfcf4f7d7ab4937ba24d6ae5
Juha Autero <jautero@iki.fi>
2014-10-25 17:32:18 +0300

    Fixed problems

--- go/readdesfire.go
+++ go/readdesfire.go
@@ -1,6 +1,11 @@
-package readdesfire
+package main
 
+import "fmt"
 import "github.com/fuzxxl/nfc/2.0/nfc"
-import "github.com/fuzxxl/freefare/0.3/freefare"
+//import "github.com/fuzxxl/freefare/0.3/freefare"
 
-device = nfc.open("pn532_uart:/dev/ttyUSB0:115200")
+func main() {
+
+	nfc_device, nfc_error := nfc.Open("pn532_uart:/dev/ttyUSB0:115200")
+	fmt.Println(nfc_device,nfc_error)
+}
