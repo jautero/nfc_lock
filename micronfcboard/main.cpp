2821118bf13007217f5b161d8b5c340fc5bf84c3
Juha Autero <jautero@iki.fi>
2015-04-14 20:51:32 +0300

    Added table of SPI pins to readme. Changed main.cpp to simple proxy between usb serial and SPI.

--- micronfcboard/main.cpp
+++ micronfcboard/main.cpp
@@ -1,11 +1,22 @@
 #include "mbed.h"
-#include "USBKeyboard.h"
 
-DigitalOut myled(LED1);
-USBKeyboard pc;
+#include "USBSerial.h"
+
+DigitalOut cs(P1_19);
+SPI spi(P0_21,P0_22,P1_15);
+USBSerial pc;
 
 int main() {
+    int ret,out;
+    cs=1;
+    spi.format(8, 0);
+    spi.frequency(1000000);
+
     while(1) {
-        pc.printf("1337 H4X0R!\n");
+        out=pc.getc();
+        cs=0;
+        ret=spi.write(out);
+        cs=1;
+        pc.putc(ret);
     }
 }
