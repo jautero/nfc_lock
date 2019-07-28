a3f2293b2946714740e958247e788fe14b6a78ed
Juha Autero <jautero@iki.fi>
2019-07-28 03:36:27 +0300

    Removed micronfcboard

--- micronfcboard/python-spi.py
+++
@@ -1,8 +0,0 @@
-import glob, serial, sys
-device=glob.glob("/dev/tty.usbmodem*")
-if not device:
-  print "Device not found. Try reseting micronfcboard"
-  sys.exit(1)
-ser=serial.serial_for_url(device[0])
-ser.write(bytearray([ 1 << 7 | 0x37 << 1,0]))
-print "%02x" % ord(ser.read(2)[1])
